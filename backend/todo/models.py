a3615b1b0386604fa2a3b6eca08b665b16656485
jordan-hoezt <jordan.hoezt@gmail.com>
2019-09-16 14:51:18 +0800

    backend done

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.utils.timezone import now, datetime
 
+now = datetime.now()
+
 # Create your models here.
 
 class Todo(models.Model):
     title = models.CharField(max_length=120, blank=False)
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
-    date = models.DateTimeField(name='Added', default=now)
+    added_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
--- backend/todo/serializers.py
+++ backend/todo/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
+from django.core.serializers.json import DjangoJSONEncoder
 from .models import Todo
 
 class Todoserializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'title', 'description', 'added', 'completed')
+        fields = ['id', 'title', 'description', 'completed', 'added_date']
+        read_only_fields = ['added_date']
