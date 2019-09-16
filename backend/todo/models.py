67e05532b86c8b8bbda557ba228d49d08812b07c
jordan-hoezt <jordan.hoezt@gmail.com>
2019-09-16 14:11:47 +0800

    added rest framework

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.timezone import datetime
+from django.utils.timezone import now, datetime
 
 # Create your models here.
 
@@ -7,7 +7,7 @@ class Todo(models.Model):
 	title = models.CharField(max_length=120, blank=False)
 	description = models.TextField(blank=True)
 	completed = models.BooleanField(default=False)
-	date = models.DateTimeField(name='Added', default=datetime.now())
+	date = models.DateTimeField(name='Added', default=now)
 
 	def __str__(self):
 		return self.title
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import Todoserializer
+from .models import Todo
 
 # Create your views here.
+class TodoView(viewsets.ModelViewSet):
+	serializer_class = Todoserializer
+	queryset = Todo.objects.all()
