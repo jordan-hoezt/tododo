727abc693a1f855092ff820600e8abd402a97f68
jordan-hoezt <jordan.hoezt@gmail.com>
2019-09-16 13:58:10 +0800

    updated todo

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.utils.timezone import datetime
 
 # Create your models here.
 
 class Todo(models.Model):
-	title = models.CharField(max_length=120)
-	descripton = models.TextField()
+	title = models.CharField(max_length=120, blank=False)
+	description = models.TextField(blank=True)
 	completed = models.BooleanField(default=False)
+	date = models.DateTimeField(name='Added', default=datetime.now())
 
 	def __str__(self):
 		return self.title
